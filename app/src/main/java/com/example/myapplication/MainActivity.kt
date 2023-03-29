70143dc46f5ec7476f7eeeae24c6cb07ea74c170
GamalSebaee <gamal@masters-ts.com>
2023-03-29 15:31:40 +0200

    update build for artifact Id

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LangUtils.applyLocale(this,"ar")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
